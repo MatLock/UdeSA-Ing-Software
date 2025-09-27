0d25babe713b32ba8608b10d497b3cc64faa9107
jorgelily <jorge@lily.ai>
2025-09-27 17:51:34 -0300

    adding calculator

--- clase_02/calc.py
+++ clase_02/calc.py
@@ -1,30 +1,32 @@
 import argparse
 
+'''
+helper function to save content in given filepath
+'''
+def save_file(file_path, result):
+  with open(file_path, "w") as f:
+    f.write(str(result) + "\n")
+    print(f"💾 operation requested saved in file {file_path}")
 
 
-def save_file(file_name, result):
-  with open(file_name, "w") as f:
-    f.write(str(result)+"\n")
-    print(f"💾 operation requested saved in file {file_name}")
-
-
+'''
+functions that parses cli command and execute operations requested
+'''
 def execute():
-    parser = argparse.ArgumentParser(description='calculator')
-    parser.add_argument('first_number', type=int, help='first number')
-    parser.add_argument('second_number', type=int, help='second number')
-    parser.add_argument('--op', choices=['+', '-', '*', '/'], help='operation')
-    parser.add_argument('--save', help='result file')
-    args = parser.parse_args()
-    
-    result = ''
-    if args.op == '+':
-      result = f'{args.first_number} + {args.second_number} = {args.first_number + args.second_number}'
-    print(result)
-    if args.save:
-      save_file(file_name=args.save, result=result)
+  parser = argparse.ArgumentParser(description='calculator')
+  parser.add_argument('first_number', type=int, help='first number')
+  parser.add_argument('second_number', type=int, help='second number')
+  parser.add_argument('--op', choices=['+', '-', '*', '/'], help='operation')
+  parser.add_argument('--save', help='result file')
+  args = parser.parse_args()
+  
+  result = ''
+  if args.op == '+':
+    result = f'{args.first_number} + {args.second_number} = {args.first_number + args.second_number}'
+  print(result)
+  if args.save:
+    save_file(file_path=args.save, result=result)
+
 
 if __name__ == '__main__':
-    execute()
-    
-    
-    
+  execute()
