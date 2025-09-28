8f98e0641534809e62e3a3420354e3ce8b581cf2
matlock <jfflores90@gmail.com>
2025-09-28 20:40:53 -0300

    [Issue-6]: adding division operation

--- clase_02/calc.py
+++ clase_02/calc.py
@@ -10,6 +10,13 @@ def save_file(file_path, result):
 
 
 '''
+given two numbers 'a' and 'b' returns string with division result
+'''
+def div(a, b):
+  return f'{a} / {b} = {a / b}'
+
+
+'''
 functions that parses cli command and execute operations requested
 '''
 def execute():
@@ -23,6 +30,8 @@ def execute():
   result = ''
   if args.op == '+':
     result = f'{args.first_number} + {args.second_number} = {args.first_number + args.second_number}'
+  if args.op == '/':
+    result = div(a=args.first_number, b=args.second_number)
   print(result)
   if args.save:
     save_file(file_path=args.save, result=result)
